516b42ee365a0360f6259afee60aacc6a0562866
Lionnus Kesting <lionnuzzz@gmail.com>
2023-12-05 16:25:34 +0100

    Instantiate modules

--- ip_repo/CCSDS_tx_ip_1.0/hdl/CCSDS_tx_ip_v1_0.v
+++ ip_repo/CCSDS_tx_ip_1.0/hdl/CCSDS_tx_ip_v1_0.v
@@ -1,11 +1,22 @@
-
+/*
+ * File: CCSDS_tx_ip_v1_0.v
+ * Author: Lionnus Kesting
+ * Date: November 22th, 2023
+ *
+ * Description: Top level module for the CCSDS transmitter IP.
+ *
+ * License: This file is licensed under the GNU GPL version 3.
+ *
+ */
+ 
 `timescale 1 ns / 1 ps
 
 	module CCSDS_tx_ip_v1_0 #
 	(
-				// Users to add parameters here
+		// Users to add parameters here
 		parameter CADU_WIDTH = 20, // Number of bytes of CADU, TF_WIDTH is CADU-4 bytes
     	parameter SPI_WIDTH = 8,
+    	parameter CLK_FREQ = 64,   // Clock frequency in MHz
 		// User parameters ends
 		// Do not modify the parameters beyond this line
 
@@ -15,7 +26,7 @@
 		parameter  SPI_RESPONSE_FAIL	= 32'h46, //Currently not implemented
 		parameter  SPI_RESPONSE_RST   	= 32'h22,
 		// The master requires a target slave base address.
-    // The master will initiate read and write transactions on the slave with base address specified here as a parameter.
+    	// The master will initiate read and write transactions on the slave with base address specified here as a parameter.
 		parameter AXI_QUAD_SPI_BASE_ADDR = 32'h44A00000,
 		parameter SPICR_OFFSET_ADDR = 32'h60, // SPI Control Register
 		parameter SPISR_OFFSET_ADDR = 32'h64, // SPI Status Register
@@ -33,37 +44,8 @@
 		// // Parameters of Axi Master Bus Interface M00_AXIS
 		parameter integer C_M00_AXIS_TDATA_WIDTH	= 16,
 		parameter integer C_M00_AXIS_START_COUNT	= 32
-
-	)
-	(
-		// Users to add ports here
-		input wire spi_rx_empty_interrupt_i,
-		// Wires for serializer
-		output wire [CADU_WIDTH*8-1:0] frame_o,
-		output wire enable_serializer_o,
-		input wire serializer_done_i,
-		
-		// Debug wires
-		output 	wire [2:0] fsm_state_o, // State of the FSM
-		input wire [3:0] debug_i,
-		output wire ccsds_read_in,
-		output wire count_debug,
-		// Debug wires serializer
-//		output  wire data_o, // Serialized output data
-//		output  wire data_valid_o, // Data valid signal
-		// Input wires for AXI4-Stream interface
-		input wire [15:0] i_data_i,
-		input wire i_data_valid_i,
-		input wire [15:0] q_data_i,
-		input wire q_data_valid_i,
-
-		// User ports ends
-		// Do not modify the ports beyond this line
-
-
+	)(
 		// Ports of Axi Master Bus Interface M00_AXI
-//		output wire  m00_axi_error,
-//		output wire  m00_axi_rxn_done,
 		input wire  m00_axi_aclk,
 		input wire  m00_axi_aresetn,
 		output wire [31: 0] m00_axi_awaddr,
@@ -86,7 +68,6 @@
 		input wire  m00_axi_rvalid,
 		output wire  m00_axi_rready,
 
-
 		// Ports of Axi Master Bus Interface M00_AXIS
 		input wire  m00_axis_aclk,
 		input wire  m00_axis_aresetn,
@@ -94,9 +75,48 @@
 		output wire [C_M00_AXIS_TDATA_WIDTH-1 : 0] m00_axis_tdata,
 		output wire [(C_M00_AXIS_TDATA_WIDTH/8)-1 : 0] m00_axis_tstrb,
 		output wire  m00_axis_tlast,
-		input wire  m00_axis_tready
+		input wire  m00_axis_tready,
 		
+		// Debug ports
+		output wire [2:0] fsm_state_o
 	);
+
+	// Wires for serializer
+	wire [CADU_WIDTH*8-1:0] frame_o;
+	wire enable_serializer_o;
+	wire serializer_done_i;
+
+	// Internal wires for interfacing with M00_AXI
+	wire m00_axi_error;
+	wire m00_axi_rxn_done;
+	wire data_o;
+	wire [3:0] debug_i;
+	wire ccsds_read_in;
+	wire count_debug;
+
+	// Wires for I data interface and serializer
+	wire valid_i;
+	wire [15:0] data_i_serializer;
+	wire valid_i_serializer;
+	wire valid_o_serializer;
+	wire [15:0] cycles_per_bit;
+
+	// Wires for the encoder
+	wire [15:0] data_o_encoder;
+	wire valid_o_encoder;
+	wire [15:0] cycles_per_bit_encoder;
+
+	// Wires for the modulator
+	wire [15:0] i_data_o;
+	wire [15:0] q_data_o;
+	wire valid_o_modulator;
+	
+	// Input wires for AXI4-Stream interface
+	wire [15:0] i_data_stream;
+	wire valid_i_stream;
+	wire [15:0] q_data_stream;
+
+	
 // Instantiation of Axi Bus Interface M00_AXI
 	CCSDS_tx_ip_v1_0_M00_AXI # ( 
 		.AXI_QUAD_SPI_BASE_ADDR(AXI_QUAD_SPI_BASE_ADDR),
@@ -154,14 +174,52 @@
 		.M_AXIS_TSTRB(m00_axis_tstrb),
 		.M_AXIS_TLAST(m00_axis_tlast),
 		.M_AXIS_TREADY(m00_axis_tready),
-		.i_data_i(i_data_i),
-		.i_valid_i(i_valid_i)
+		.i_data_i(i_data_stream),
+		.q_data_i(q_data_stream),
+		.valid_i(valid_stream)
+	);
+// Instantiate the serializer
+	tx_serializer #(
+		.CADU_WIDTH(CADU_WIDTH)
+	) serializer (
+		.clk_i(clk_i),
+		.rst_ni(rst_ni),
+		.data_i(data_i_serializer),
+		.valid_i(valid_i_serializer),
+		.valid_o(valid_o_serializer),
+		.cycles_per_bit(cycles_per_bit)
 	);
 
-// TODO: Instantiate the convolutional encoder module
-
-// TODO: Implement framing
-
-// TODO: Implement modulator
+// Instantiate the convolutional encoder module
+	ccsds_convolutional_encoder #(
+		.K(7),
+		.G1(7'o171),
+		.G2(7'o133)
+	) encoder (
+		.clk_i(clk_i),
+		.rst_ni(rst_ni),
+		.data_i(data_i_serializer),
+		.data_o(data_o_encoder),
+		.valid_i(valid_o_serializer),
+		.valid_o(valid_o_encoder),
+		.cycles_per_bit_i(cycles_per_bit),
+		.cycles_per_bit_o(cycles_per_bit_encoder)
+	);
+// TODO: Implement framing module
+// Instantiate the modulator module
+	ccsds_modulator # (
+		.CLK_FREQ(CLK_FREQ),
+		.SAMPLE_RATE(1)
+//		.MOD_TYPE(0)
+	) modulator (
+		.clk_i(clk_i),  
+		.rst_ni(rst_ni),
+		.bit_i(data_o_encoder),
+		.i_data_o(i_data_o),
+		.q_data_o(q_data_o),
+		.cycles_per_bit(cycles_per_bit_encoder),
+		.valid_i(valid_o_encoder),
+		.valid_o(valid_o_modulator)
+	);
 
 	endmodule
